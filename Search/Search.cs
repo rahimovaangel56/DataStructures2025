ab25699e70c5a20a3d9d37f34a0640d586b18360
Elena <egurnovaelena56@gmail.com>
2025-09-23 14:32:08 +0500

    sd

--- Search/Search.cs
+++ Search/Search.cs
@@ -20,6 +20,19 @@ namespace Search
 
         public static int IterativeBinarySearch(int key, params int[] values)
         {
+            int left = 0;
+            int right = values.Length - 1;
+
+            while (left <= right)
+            {
+                int middle = (left + right) / 2;
+                if (values[middle] == key)
+                    return middle;
+                else if (values[middle] > key)
+                    right = middle -1;
+                else 
+                    left = middle + 1;
+            }
             return -1;
         }
 
