7b3f5384ff1038c239f67d69e86b4612820582f9
marine mono <monomarinee@gmail.com>
2025-09-24 18:11:38 +0500

    обход в глубину в графе на матрице смежности

---
+++ Graphs/GraphByMatrix.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphs
+{
+    internal class GraphByMatrix
+    {
+        private int size;
+        private int[,] adjacency;
+        private bool[] vector;
+
+        public GraphByMatrix(int[,] adj)
+        {
+
+            if(adj.GetLength(0) == adj.GetLength(1))
+            {
+                size = adj.GetLength(0);
+   
+                    adjacency = adj;
+                vector = new bool[size];
+            }
+        }
+        public void Depth(int vertex)
+        {
+            vector[vertex] = true;
+            Console.WriteLine(vertex);
+            for(int j = 0; j < size; j++)
+            {
+                if(adjacency[vertex,j] != 0 && !vector[j])
+                    Depth(j);
+            }
+        }
+    }
+}
---
+++ Graphs/Program.cs
@@ -0,0 +1,19 @@
+﻿using Graphs;
+
+namespace Graph
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] a = { { 0,0,0,0},
+                         { 0,0,1,0},
+                         { 1,0,0,0},
+                         { 1,0,1,0}
+                        };
+
+            GraphByMatrix graph = new GraphByMatrix(a);
+            graph.Depth(0);
+        }
+    }
+}
