b190e698cf6239a98718b17a2748a5df3406e15c
marine mono <monomarinee@gmail.com>
2025-09-25 12:25:56 +0500

    gr1

---
+++ Graphs/GraphByList.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphs
+{
+    internal class GraphByList
+    {
+        private Node root; //самый первый узел добавленный в граф
+        private HashSet<Node> vector;
+
+        public GraphByList(string rootValue)
+        {
+            this.root = new Node(rootValue);
+        }
+
+        public Node AddNode(string value, Node parent = null)
+        {
+            Node newNode = new Node(value);
+            if(parent == null)
+            {
+                newNode.Neighbors.Add(root);
+                root.Neighbors.Add(newNode);
+            }
+            else
+            {
+                newNode.Neighbors.Add(parent);
+                parent.Neighbors.Add(newNode);
+            }
+            return newNode;
+        }
+
+        public void RemoveNode(Node node)
+        {
+            if (node == null) return;
+            foreach (Node child in node.Neighbors)
+            {
+                child.Neighbors.Remove(node);
+            }
+            node.Neighbors.Clear();
+            node = null;
+        }
+    }
+}
---
+++ Graphs/Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphs
+{
+    internal class Node
+    {
+        public string Value { get; set; }
+        public List<Node> Neighbors { get; set; }
+
+        public Node(string value)
+        {
+            Value = value;  
+            Neighbors = new List<Node>();
+        }
+
+        public override string ToString() => Value;
+    }
+}
