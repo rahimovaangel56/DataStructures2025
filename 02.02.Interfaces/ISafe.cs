c9c643379f4bb3abf708c42fdc556b0ae6151b95
Elena <egurnovaelena56@gmail.com>
2025-09-29 15:23:58 +0500

    s

---
+++ 02.02.Interfaces/ISafe.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02._02.Interfaces
+{
+    internal interface ISave : IPurse
+    {
+        bool IsLocked { get; }
+        void Lock();
+        void Unlock();
+    }
+}
---
+++ 02.02.Interfaces/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _02._02.Interfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
