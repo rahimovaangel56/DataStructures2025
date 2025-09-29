03145842c72ed6ca159cfca6bec790a79b8c3b5c
Elena <egurnovaelena56@gmail.com>
2025-09-29 15:34:27 +0500

    save

--- 02.02.Interfaces/Save.cs
+++ 02.02.Interfaces/Save.cs
@@ -6,7 +6,43 @@ using System.Threading.Tasks;
 
 namespace _02._02.Interfaces
 {
-    internal class Save
+    internal class Save : ISave
     {
+        private bool isLocked = true;
+        private int summ = 0;
+        public bool IsLocked => isLocked;
+
+        public int Summ
+        {
+            get => summ;
+            set => summ = value;
+        }
+
+        public void AddMoney(int amount)
+        {
+            if (amount > 0 && !IsLocked)
+                Summ += amount;
+            else if (IsLocked)
+                throw new ArgumentException("Нельзя пополнить счет");
+        }
+
+        public int DecMoney(int amount)
+        {
+            if (Summ > amount && !IsLocked)
+                return Summ -= amount;
+            else if (IsLocked) throw new ArgumentException("Нельзя снять деньги");
+            else
+                throw new ArgumentException("Недостаточно средств");
+        }
+
+        public void Lock()
+        {
+            if(!isLocked) isLocked = true;
+        }
+
+        public void Unlock()
+        {
+            if(isLocked) isLocked = false;
+        }
     }
 }
