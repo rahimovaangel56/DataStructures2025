1d90268e643117b588acfab7796021dcafb75f69
aereo <aereo@PC1-ROOM409>
2025-10-16 16:39:03 +0500

    factoryMethod

---
+++ FactoryMethod/FileLogger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    internal class FileLogger : ILogger
+    {
+        private readonly string _lofFiliName = "log.txt";
+        public void WrileLog(string message)
+        {
+            File.AppendAllText(_lofFiliName, message);
+        }
+    }
+}
---
+++ FactoryMethod/LoggerFactory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    internal abstract class LoggerFactory
+    {
+        public abstract ILogger Createlogger();
+
+        public void Log(string message)
+        {
+            var logger = Createlogger();
+            logger.WrileLog(message);
+        }
+    }
+
+    internal class FileLoggerFactory : LoggerFactory
+    {
+        public override ILogger Createlogger()
+        {
+            return new FileLogger();
+        }
+    }
+
+    internal class ConsoleLoggerFactory : LoggerFactory
+    {
+        public override ILogger Createlogger()
+        {
+            return new ConsoleLogger();
+        }
+    }
+}
